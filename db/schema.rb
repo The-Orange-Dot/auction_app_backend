fa5ed4260abd3b450ab8c228b8d77a258c1b0330
TheOrangeDot <theorangedottwitch@gmail.com>
2022-02-08 04:38:28 +0900

    postgres conversion

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_28_120841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "category"
     t.string "name"
