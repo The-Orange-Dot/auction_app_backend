cc83be93c35f27366000e64cb88cfe314302c87c
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-10 04:25:09 +0900

    revert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user
       # session[:user_id] ||= user.id
-      cookies.signed[:user_id] ||= 25
+      cookies.signed[:user_id] ||= user.id
       # session[:user_id] = user.id
-      session[:user_id] ||= 25
+      session[:user_id] ||= user.id
+
       puts "SessionController: #{session}"
       puts "SessionController: #{session[:user_id]}"
       
