b00f9b30a03ad0de3b971ee9c9992a45e0374968
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-09 22:38:56 +0900

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ActiveRecord::Base
   has_many :products
 
   validates :email, uniqueness: true
+  validates :email, presence: true
   validates :username, uniqueness: true
+  validates :username, presence: true
   validates :password, length: {minimum: 8}
+  validates :password, presence: true
+
 
   # This breaks the charge_points controller for some reason
   # validates :username, length: {in: 6..15}
