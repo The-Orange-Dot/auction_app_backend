7d54c286e3cbd8cb96b5d9ce9330e80a46392594
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-10 21:13:26 +0900

    User validation added

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,16 @@ class Product < ActiveRecord::Base
   belongs_to :user
 
 validates :name, presence: true
+validates :category, presence: true
 validates :price, presence: true
-validates :price, numericality: {greater_than_or_equal_to: 2000}
+validates :price, numericality: {greater_than_or_equal_to: 1000}
 validates :tickets, presence: true
+validates :tickets, numericality: {in: 3..50}
+validates :description, length: {maximum: 300}
+validates :description, resence: true
+validates :descriptionPreview, length: {maximum: 120}
+validates :descriptionPreview, presence: truess
+validates :keywords, length: {maximum: 50}
 
 
 
