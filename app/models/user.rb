30efd38dc838d57385e827cfecf20e5c24f94323
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-10 19:27:58 +0900

    fixed validation typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   # has_secure_password
   has_many :products
 
-  validates :email, uniqueness:  {true, message: "Email is already in use"}
+  validates :email, uniqueness:  { message: "Email is already in use"}
   validates :email, presence: true
-  validates :username, uniqueness: {true, message: "%{value} has already been taken"}
+  validates :username, uniqueness: { message: "%{value} has already been taken"}
   validates :username, presence: true
   validates :password, numericality: {greater_than_or_equal_to: 8, message: "Password must be at least 8 characters long"}
   validates :password, presence: true
