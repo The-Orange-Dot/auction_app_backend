4cbddc8652a1c29763678f1c58173f68f4579dc8
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-11 02:30:30 +0900

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :user]
+  resources :users, only: [:create, :user, :patch]
   resources :products, except: [:update, :show]
   resource :sessions, only: [:create, :destroy]
 
