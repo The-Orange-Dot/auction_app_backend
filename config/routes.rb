409fb98e127f3326791e81d9f9c79f0195a77749
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-17 02:13:11 +0900

    fixed update again...

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :user]
+  resources :users, only: [:create, :user, :update]
   resources :products, except: [:update, :show]
   resource :sessions, only: [:create, :destroy]
 
   patch "users/buy_ticket/:id" => "users#buy_ticket"
   patch "users/charge_points/:id" => "users#charge_points"
-  patch "users/:id" => "users#update"
   get "user" => "users#show"
 
   post "/login" => "sessions#create"
