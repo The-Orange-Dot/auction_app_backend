a2c1747cd2bf8c5f40b6d26cf6f45e73ca63e572
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-02-17 02:11:52 +0900

    fixed patch route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :user, :update]
+  resources :users, only: [:create, :user]
   resources :products, except: [:update, :show]
   resource :sessions, only: [:create, :destroy]
 
   patch "users/buy_ticket/:id" => "users#buy_ticket"
   patch "users/charge_points/:id" => "users#charge_points"
+  patch "users/:id" => "users#update"
   get "user" => "users#show"
 
   post "/login" => "sessions#create"
