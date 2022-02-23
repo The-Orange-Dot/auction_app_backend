17e8fd6baa7641702aa51f253628f0f79d1c9d6f
TheOrangeDot <theorangedottwitch@gmail.com>
2022-02-24 04:59:06 +0900

    CORS fix?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*'
+    origins '*',
+    resource '*',
       :headers => :any,
       :methods => [:get, :post, :put, :patch, :delete, :options, :head]
   end
