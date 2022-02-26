25d73a684ee6af96435efac1ba99b02ee3279186
TheOrangeDot <theorangedottwitch@gmail.com>
2022-02-26 16:22:56 +0900

    added create controller to reviews

--- app/controllers/buyer_reviews_controller.rb
+++ app/controllers/buyer_reviews_controller.rb
@@ -1,2 +1,20 @@
 class BuyerReviewsController < ApplicationController
+  def index
+    render json: BuyerReview.all, status: :ok
+  end
+
+  def create
+    review = BuyerReview.create(buyer_review_params)
+    if user.valid?
+      render json: review, status: :created
+    else
+      redner json: {error: "Invalid input"}
+    end
+  end
+
+  private
+
+  def buyer_review_params
+    params.permit(:id, :review, :rating, :user_id, :seller_id, :product_id)
+  end
 end
--- app/controllers/seller_reviews_controller.rb
+++ app/controllers/seller_reviews_controller.rb
@@ -3,4 +3,20 @@ class SellerReviewsController < ApplicationController
     render json: SellerReview.all, status: :ok
   end
 
+
+  def create
+    review = SellerReview.create(seller_review_params)
+    if user.valid?
+      render json: review, status: :created
+    else
+      redner json: {error: "Invalid input"}
+    end
+  end
+
+  private
+
+  def seller_review_params
+    params.permit(:id, :review, :rating, :user_id, :buyer_id, :product_id)
+  end
+
 end
