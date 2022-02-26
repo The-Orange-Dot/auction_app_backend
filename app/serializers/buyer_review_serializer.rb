723dfef7032041d663fa38bedada1701cd196480
TheOrangeDot <theorangedottwitch@gmail.com>
2022-02-26 17:38:42 +0900

    serializer for reviews fixed

--- app/serializers/buyer_review_serializer.rb
+++ app/serializers/buyer_review_serializer.rb
@@ -1,5 +1,9 @@
 class BuyerReviewSerializer < ActiveModel::Serializer
-  attributes :id, :rating, :review
+  attributes :id, :rating, :review, :seller_id
   has_one :user
   has_one :product
+
+  def seller_id
+    User.find(self.object.seller_id)
+  end
 end
--- app/serializers/seller_review_serializer.rb
+++ app/serializers/seller_review_serializer.rb
@@ -1,5 +1,9 @@
 class SellerReviewSerializer < ActiveModel::Serializer
-  attributes :id, :review, :rating
+  attributes :id, :review, :rating, :buyer_id
   has_one :user
   has_one :product
+
+  def buyer_id
+    User.find(self.object.buyer_id)
+  end
 end
