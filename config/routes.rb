bbcbfd7e0066474900943881386c1f99997938d2
TheOrangeDot <theorangedottwitch@gmail.com>
2022-03-02 03:07:02 +0900

    Added show route to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   resources :buyer_reviews, only: [:create, :index]
   resources :seller_reviews, only: [:create, :index]
-  resources :users, only: [:index, :create, :user, :update]
+  resources :users, only: [:show, :index, :create, :user, :update]
   resources :products, except: [:update, :show]
   resource :sessions, only: [:create, :destroy]
 
   post "users/buy_ticket/:id" => "users#buy_ticket"
   post "users/charge_points/:id" => "users#charge_points"
   post "users/edit_info/:id" => "users#update"
-
-  get "user" => "users#show"
 
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
